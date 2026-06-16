e40a2bce588481281cd3ff26abee4ceaa319a8d4
kondwanDev <kondwan03@gmail.com>
2026-06-16 08:55:13 +0200

    string amount

--- practice/schemas/book.py
+++ practice/schemas/book.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class Book (BaseModel):
-    title : str
-    author : str
+    title : str = Field (min_length= 1)
+    author : str = Field (min_length= 1)
     year : int = Field (ge= 1900, le= 2026)
 
 class UpdatedBook (BaseModel):
-    title : Optional[str] = None
-    author : Optional[str] = None
+    title : Optional[str] = Field(default= None, min_length= 1 )
+    author : Optional[str] = Field(default= None, min_length= 1 )
     year : Optional [int]  = Field (default=None, ge= 1900, le= 2026)
